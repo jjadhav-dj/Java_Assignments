8565568b0a94dbb1a051e7c429d3f0af39de8b4e
atipune <dnyanraj88>
2025-09-29 22:15:59 +0530

    Initial commit of project files

---
+++ src/Assignment_Day1/DataTypes_Assignment.java
@@ -0,0 +1,25 @@
+package Assignment_Day1;
+
+public class DataTypes_Assignment {
+
+	public static void main(String[] args) {
+//		1. Temperature of a city in degrees Celsius: 25.5
+		
+		float num = 25.5f;
+		System.out.println("Temperature of a city:"+ num);
+		
+//		2. Whether a customer has placed an order: true or false
+		boolean orderStatus1 = true;
+		boolean orderStatus2 = false;
+		System.out.println("customer has placed an order:"+ orderStatus1);
+		System.out.println("customer has placed an order:"+ orderStatus2);
+		
+//		3. Person's phone number: "123-456-7890"
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/languageBasics/ArrayExamples.java
@@ -0,0 +1,10 @@
+package languageBasics;
+
+public class ArrayExamples {
+
+	public static void main(String[] args) {
+		String [] EmpName = { 
+
+	}
+
+}
